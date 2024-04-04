2b0f59a29d8331d448ebc90df497771fb2ba189e
Aleš Krejzl <ales.krejzl@Ales-MacBook-Air.local>
2024-04-04 07:26:22 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className="border text-red-400">
+      <h1 className="text-red-200">Fck</h1>
+    </div>
+  )
+}
+
+export default App
