73a7dc49aab15212878b0587ac9cbc3a98bb0b81
Aleš Krejzl <ales.krejzl@gmail.com>
2024-04-04 10:36:58 +0200

    add navbar and links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
@@ -11,6 +12,7 @@ import Projects from './pages/Projects'
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
