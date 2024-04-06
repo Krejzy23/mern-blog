0ee0b806729d6298f062f1740e0f24c7e2e29b74
Aleš Krejzl <ales.krejzl@gmail.com>
2024-04-06 10:08:13 +0200

    complete user image upload functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,8 +15,15 @@ export default function Header() {
     const { theme } = useSelector((state) => state.theme)
   return (
     <Navbar className='border-b-2'>
-      <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Krejzy</span>
+      <Link 
+        to="/" 
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span 
+          className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'
+        >
+          Krejzy
+        </span>
         Blog
       </Link>
       <form>
