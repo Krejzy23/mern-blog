1ce0758803e9da9173e4e638093d2ee30d10f4b9
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-08 23:07:33 +0200

    json clearing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +11,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(),svgr()],
+  plugins: [react()],
 })
