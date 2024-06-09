01ecb4e5d8d5d4ab44fa36b5916c86b8d22d5aad
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-09 22:55:05 +0200

    UI respon Projects

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Section from "../components/Section";
 import { BottomLine } from "../components/design/Hero";
 import TextSlider from "../components/TextSlider";
+import Tagline from "../components/Tagline";
 
 export default function About() {
   return (
     <div>
     <Section className="pt-[5rem] mt-[2.25rem]" crosses paddings>
       <div className="relative max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem]">
+      <Tagline className="text-xl">ABOUT ME</Tagline>
         <div className="flex justify-center items-center">
           <TextSlider />
         </div>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center">
-          <h1 className="text-transparent text-stroker-1 text-stroke-custom mt-20 font-semibold text-4xl tracking-wider font-poppins">
+          <h1 className="text-transparent text-stroker-1 text-stroke-custom mt-10 font-semibold text-4xl lg:text-6xl tracking-wider font-poppins">
             About Me
           </h1>
           <p className="font-semibold px-3">
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -16,16 +16,16 @@ const Projects = () => {
         <Tagline className="text-xl">MY PROJECTS</Tagline>
           <h1 className="h1 body-1 font-poppins mt-20 ml-20">
             A small selection of my{" "}
-            <h2 className="flex justify-center items-center text-4xl md:text-6xl font-semibold text-stroke-1 font-poppins text-transparent text-stroker-1 text-stroke-custom">Recent projects</h2>
+            <h1 className="flex h1 justify-center items-center font-semibold text-stroke-1 font-poppins text-transparent text-stroker-1 text-stroke-custom">Recent projects</h1>
           </h1>
           <div className="grid grid-cols-1 md:grid-cols-2 p-4 gap-16 mt-10">
             {projects.map((item) => (
               <div
                 key={item.id}
-                className="flex relative lg:min-h-[32.5rem] h-[30rem] items-center justify-center"
+                className="flex relative lg:min-h-[32.5rem] h-[28rem] items-center justify-center"
               >
                 <div className="border border-stroke-1 rounded-2xl flex flex-col w-full h-full overflow-hidden p-5">
-                  <div className="relative flex items-center justify-center w-full overflow-hidden sm:h-[40vh] h-[30vh] lg:h-[30vh] mb-10">
+                  <div className="relative flex items-center justify-center w-full overflow-hidden sm:h-[40vh] h-[35vh] lg:h-[30vh] mb-10">
                     <div
                       className="relative w-full h-full justify-center items-center overflow-hidden rounded-3xl"
                       style={{ backgroundColor: "#13162D" }}
